4fcac80d296450ad5e085f3e2f9fb201691141be
edavis8 <ethan.davis@uvm.edu>
2020-08-30 11:22:23 -0500

    steps 48-51

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,9 +1,17 @@
 var controllerOptions = {};
 var i = 0;
+var x = window.innerWidth/2;
+var y = window.innerHeight/2;
 Leap.loop(controllerOptions, function(frame)
 {
-console.log(i);
-i+=1;
+clear()
+circle(x,y,50);
+var dx = Math.floor(Math.random()*3)-1;
+var dy = Math.floor(Math.random()*3)-1;
+x+=dx;
+y+=dy;
+//console.log(i);
+//i+=1;
 }
-);
+)
 
--- prepareToDraw.js
+++ prepareToDraw.js
@@ -1,6 +1,3 @@
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  var x = window.innerWidth/2;
-  var y = window.innerHeight/2;
-  circle(x, y, 50);
 }
