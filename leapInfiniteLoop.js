0b3305e81ea4739c8ea4034895d72f0999c60dc6
edavis8 <ethan.davis@uvm.edu>
2020-08-30 11:39:39 -0500

    steps 53-55

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -4,14 +4,18 @@ var x = window.innerWidth/2;
 var y = window.innerHeight/2;
 Leap.loop(controllerOptions, function(frame)
 {
-clear()
-circle(x,y,50);
-var dx = Math.floor(Math.random()*3)-1;
-var dy = Math.floor(Math.random()*3)-1;
-x+=dx;
-y+=dy;
+//clear()
+//circle(x,y,50);
+//var dx = Math.floor(Math.random()*3)-1;
+//var dy = Math.floor(Math.random()*3)-1;
+//x+=dx;
+//y+=dy;
 //console.log(i);
 //i+=1;
+if (frame.hands.length == 1) {
+    console.log(frame.hands);
+}
+    
 }
 )
 
